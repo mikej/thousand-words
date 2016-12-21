6b6e29e075ff23cec15a29fc942f8b5f500083c6
Michael Josephson <mike@josephson.org>
2016-12-21 10:20:36 +0000

    re-add Storyboard and Table VC by hand

---
+++ Thousand Words/AlbumTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AlbumTableViewController.h
+//  Thousand Words
+//
+//  Created by Michael Josephson on 21/12/2016.
+//  Copyright © 2016 Michael Josephson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlbumTableViewController : UITableViewController
+
+@end
