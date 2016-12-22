04fa36a495b47f32016e7317f7c608b4de286923
Michael Josephson <mike@josephson.org>
2016-12-22 10:57:43 +0000

    add UICollectionViewController

---
+++ Thousand Words/PhotosCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotosCollectionViewController.h
+//  Thousand Words
+//
+//  Created by Michael Josephson on 22/12/2016.
+//  Copyright © 2016 Michael Josephson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotosCollectionViewController : UICollectionViewController
+
+@end
